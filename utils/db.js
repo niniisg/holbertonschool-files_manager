214da1e601fc17aeab0e2afa1e391188b6fce9ef
niniisg <nisselsanchez@gmail.com>
2025-04-13 13:24:15 -0400

    task 2 Firts API

---
+++ utils/db.js
@@ -0,0 +1,43 @@
+import mongodb from 'mongodb';
+
+const { MongoClient } = mongodb;
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || '27017';
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.db = null;
+    this.connected = false;
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(DB_DATABASE);
+        this.connected = true;
+      })
+      .catch((err) => {
+        console.error('MongoDB connection error:', err);
+      });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    if (!this.db) return 0;
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.db) return 0;
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
