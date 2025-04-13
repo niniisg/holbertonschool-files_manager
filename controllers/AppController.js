083ce7ca341d632bd60f79fea0650a1607e28784
niniisg <nisselsanchez@gmail.com>
2025-04-13 13:29:18 -0400

    fixing eslint

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,11 +1,11 @@
-import dbClient from '../utils/db.js';
-import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AppController {
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive()
+      db: dbClient.isAlive(),
     });
   }
 
