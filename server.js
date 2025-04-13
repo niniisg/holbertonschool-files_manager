6bc778cbc29243d1766a1eac686a288557cf6ac9
niniisg <nisselsanchez@gmail.com>
2025-04-13 13:34:02 -0400

    fixing eslint

--- server.js
+++ server.js
@@ -7,6 +7,5 @@ const port = process.env.PORT || 5000;
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-
+  console.log(`Server listening on port ${port}`);
 });
