638bd0bf2531f1f086b0f8efa4a23d856ec18321
niniisg <nisselsanchez@gmail.com>
2025-04-13 19:03:22 -0400

    addig missing import

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
+import sha1 from 'sha1';
 
 
 class UsersController {
@@ -15,20 +16,18 @@ static async postNew(req, res){
     }
 
     try {
-        const existingUser = await User.findOne({ email });
-    
+        const existingUser = await dbClient.db.collection('users').findOne({ email });
         if (existingUser) {
           return res.status(400).json({ error: 'Already exist' });
         }
         const hashedPassword = sha1(password);
-      
-        // Create new user
+ 
         const result = await dbClient.db.collection('users').insertOne({
           email,
           password: hashedPassword,
         });
   
-        // Return the new user with email and id
+      
         return res.status(201).json({
           id: result.insertedId,
           email,
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ import UsersControllers from '../controllers/UsersController';
 
 const router = express.Router();
 
+
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-router.post('/users', UsersControllers.createUser)
+
+
+router.post('/users', UsersControllers.postNew);
 
 export default router;
